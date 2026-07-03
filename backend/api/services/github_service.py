3bae792eacad47a3626af95fe7a3432e0f6937cb
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-03 16:40:03 -0600

    feat: create structure to the new github

---
+++ backend/api/services/github_service.py
@@ -0,0 +1,41 @@
+import requests
+from urllib.parse import urlparse
+
+class GitHubService:
+    BASE_URL = "https://api.github.com/repos"
+
+    @staticmethod
+    def extract_owner_repo(repository_url):
+
+        parsed = urlparse(repository_url)
+
+        path = path.strip("/")
+
+        parts = path.split("/")
+
+        if  len(parts) < 2:
+            raise ValueError("The repository URL is invalid. ")
+        owner = parts[0]
+        repository = parts[1]
+
+        return owner, repository
+    
+
+    @classmethod
+    def get_repository(cls, repository_url):
+
+        owner, repository = cls.extract_owner_repo(repository_url)
+        
+        url = f"{cls.BASE_URL}/{owner}/{repository}"
+
+        response = requests.get(url)
+
+        if response.status_code == 404:
+            raise Exception("Repositorio no encontrado.")
+
+        if response.status_code != 200:
+            raise Exception(
+                f"GitHub API respondió con {response.status_code}"
+            )
+
+        return response.json()
