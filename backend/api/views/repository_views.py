a1f09e1d366f6e60fbb35570e6f6d140804bfb4a
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-04 19:04:31 -0600

    feat: create new structure for the repository analyzer view in the repository_views.py

---
+++ backend/api/views/repository_views.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from api.services import GitHubService
+
+
+class RepositoryAnalyzerView(APIView):
+
+    def post(self, request):
+
+        repository_url = request.data.get("url")
+
+        if not repository_url:
+            return Response(
+                {"error": "Repository URL is required. "},
+                status = status.HTTP_400_BAD_REQUEST
+            ) 
+        
+        try:
+            
+            repository_data = GitHubService.get_repository(repository_url)
+
+            return Response(repository_data)
+        
+        except Exception as e: 
+
+            return Response(
+                {"error": str(e)},
+                status = status.HTTP_400_BAD_REQUEST    
+            )
