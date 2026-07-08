6a0de5ad83973f55bee80f92d0f188ea23f28df9
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-07 19:51:33 -0600

    fix: update repository_score for the popularity structure in api/utils

--- backend/api/utils/score_calculator.py
+++ backend/api/utils/score_calculator.py
@@ -3,18 +3,20 @@ class RepositoryScore:
     @staticmethod
     def popularity(stars):
 
-        if stars > 100000:
+        if stars >= 100000:
             return 100
 
-        if stars > 50000:
+        if stars >= 50000:
             return 90
 
-        if stars > 10000:
+        if stars >= 10000:
             return 80
 
-        if stars > 1000:
+        if stars >= 1000:
             return 70
 
         return 50
+    
+        @staticmethod
     
     
