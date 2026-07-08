149623cb0683fa4ba746567a0b0db03b323cbbf1
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-07 20:09:49 -0600

    fix: update RepositoryClassifier for the classify new structure in api/utils

--- backend/api/utils/repository_classifier.py
+++ backend/api/utils/repository_classifier.py
@@ -6,19 +6,33 @@ class RepositoryClassifier:
         text = f"{language} {description}".lower()
 
         if "react" in text:
-
+            return "Frontend"
+        
+        if "vue" in text:
+            return "Frontend"
+        
+        if "angular" in text:
             return "Frontend"
 
         if "django" in text:
-
+            return "Backend"
+        
+        if "flask" in text:
+            return "Backend"
+        
+        if "fastapi" in text:
             return "Backend"
 
         if "machine learning" in text:
-
             return "Artificial Intelligence"
 
         if "tensorflow" in text:
-
             return "Artificial Intelligence"
+        
+        if "pytorch" in text:
+            return "Articial Intelligence"
+        
+        if "docker" in text:
+            return "DevOps"
 
         return "General"
