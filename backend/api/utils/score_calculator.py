6918a6bc5ad6b9aa9c61987e48323b01d19da7a8
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-07 19:52:33 -0600

    fix: update repository_score for the activity structure in api/utils

--- backend/api/utils/score_calculator.py
+++ backend/api/utils/score_calculator.py
@@ -17,6 +17,20 @@ class RepositoryScore:
 
         return 50
     
-        @staticmethod
+
+    @staticmethod
+    def activity(open_issues):
+        if open_issues < 25:
+            return 100
+        
+        if open_issues < 100:
+            return 85
+        
+        if open_issues < 500:
+            return 70
+        
+        return 50
     
+
+
     
