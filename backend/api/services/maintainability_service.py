f1cd83e35fbc20a9a84c05f296137b090684176c
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-09 18:28:03 -0600

    feat: create structure for the maintainability_service.py in the api/services

---
+++ backend/api/services/maintainability_service.py
@@ -0,0 +1,31 @@
+class MaintainabilityService:
+
+    @staticmethod
+    def calculate(language, stars, forks, open_issues):
+
+        score = 50
+
+        if language in [
+            "Python",
+            "JavaScript",
+            "TypeScript",
+            "Java",
+            "C#",
+            "Go",
+            "Rust",
+        ]:
+            score += 15
+
+        if stars >= 1000:
+            score += 10
+
+        if forks >= 100:
+            score += 10
+
+        if open_issues <= 100:
+            score += 15
+
+        if score > 100:
+            score = 100
+
+        return score
