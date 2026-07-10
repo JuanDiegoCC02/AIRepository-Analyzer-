771d5d415f78bc4cb4de3f33232eea395fad9672
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-09 18:41:33 -0600

    fix: changes imports structure __init__.py in the api/services

--- backend/api/services/__init__.py
+++ backend/api/services/__init__.py
@@ -1,2 +1,5 @@
 from .github_service import GitHubService
 from .analyzer_service import AnalyzerService
+from .documentation_service import DocumentationService
+from .maintainability_service import MaintainabilityService
+from .overall_score_service import OverallScoreService
