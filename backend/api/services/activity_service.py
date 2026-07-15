ece1fd7f4852e0ed0dbe14220387b5885b7c2bd5
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-15 15:40:15 -0600

    fix: update the ActivityService structure to activity_service.py archive in api/services

--- backend/api/services/activity_service.py
+++ backend/api/services/activity_service.py
@@ -1,11 +1,31 @@
-
+from datetime import datetime, timezone
 
 
 class ActivityService:
 
-       @staticmethod
-       def calculate(updated_at):
-              
-              #future logic structure
+    @staticmethod
+    def calculate(repository):
 
-              return 100
+        updated_at = repository.get("updated_at")
+
+        updated = datetime.fromisoformat(
+            updated_at.replace("Z", "+00:00")
+        )
+
+        now = datetime.now(timezone.utc)
+
+        days = (now - updated).days
+
+        if days <= 7:
+            return 100
+
+        if days <= 30:
+            return 80
+
+        if days <= 90:
+            return 60
+
+        if days <= 180:
+            return 40
+
+        return 20
