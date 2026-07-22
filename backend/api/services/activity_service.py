9181d967190546292d8a3fd32c3087518e41820c
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-22 00:03:36 -0600

    fix: update structure to activity_service.py archive in api/services

--- backend/api/services/activity_service.py
+++ backend/api/services/activity_service.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timezone
+from datetime import datetime
+from django.utils import timezone
 
 
 class ActivityService:
@@ -6,26 +7,83 @@ class ActivityService:
     @staticmethod
     def calculate(repository):
 
+        score = 0
+
+       
+        # Last update
         updated_at = repository.get("updated_at")
 
-        updated = datetime.fromisoformat(
-            updated_at.replace("Z", "+00:00")
+        if updated_at:
+
+            updated_date = datetime.fromisoformat(
+                updated_at.replace("Z", "+00:00")
+            )
+
+            days = (
+                timezone.now() - updated_date
+            ).days
+
+            if days <= 30:
+                score += 40
+
+            elif days <= 90:
+                score += 30
+
+            elif days <= 180:
+                score += 20
+
+            else:
+                score += 10
+
+       
+        # Watchers
+        watchers = repository.get(
+            "watchers_count",
+            0,
         )
 
-        now = datetime.now(timezone.utc)
+        if watchers >= 10000:
+            score += 20
 
-        days = (now - updated).days
+        elif watchers >= 1000:
+            score += 15
 
-        if days <= 7:
-            return 100
+        elif watchers >= 100:
+            score += 10
 
-        if days <= 30:
-            return 80
+       
+        # Forks
+        forks = repository.get(
+            "forks_count",
+            0,
+        )
 
-        if days <= 90:
-            return 60
+        if forks >= 10000:
+            score += 20
 
-        if days <= 180:
-            return 40
+        elif forks >= 1000:
+            score += 15
 
-        return 20
+        elif forks >= 100:
+            score += 10
+
+       
+        # Open Issues
+        issues = repository.get(
+            "open_issues_count",
+            0,
+        )
+
+        if issues <= 100:
+            score += 20
+
+        elif issues <= 500:
+            score += 15
+
+        elif issues <= 1000:
+            score += 10
+
+        else:
+            score += 5
+
+        return min(score, 100)
