9f8651c779b10b30620ad552002e5d28f63056f8
Juan Diego C.C. <dcorella2324@gmail.com>
2026-07-24 19:40:00 -0600

    feat: create the CodeQualityService new structure to code_quality_service.py archive in api/services

---
+++ backend/api/services/code_quality_service.py
@@ -0,0 +1,54 @@
+class CodeQualityService:
+
+    @staticmethod
+    def calculate(repository):
+
+        score = 0
+
+        # Issues
+
+        issues = repository.get(
+            "open_issues_count",
+            0
+        )
+
+        if issues <= 25:
+            score += 30
+
+        elif issues <= 100:
+            score += 20
+
+        else:
+            score += 10
+
+        # Wiki
+
+        if repository.get("has_wiki"):
+            score += 10
+
+        # Projects
+
+        if repository.get("has_projects"):
+            score += 10
+
+        # Discussions
+
+        if repository.get("has_discussions"):
+            score += 10
+
+        # License
+
+        if repository.get("license"):
+            score += 20
+
+        # Default Branch
+
+        if repository.get("default_branch") == "main":
+            score += 10
+
+        # Archived
+
+        if not repository.get("archived"):
+            score += 10
+
+        return min(score, 100)
