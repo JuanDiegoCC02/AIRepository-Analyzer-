79d52bb16225db74f66021b5469543c21acd80a4
Juan Diego C.C. <dcorella2324@gmail.com>
2026-08-02 17:18:41 -0600

    fix: update the imports for __init__.py in api/services

--- backend/api/services/__init__.py
+++ backend/api/services/__init__.py
@@ -2,6 +2,7 @@
 
 from .github_service import GitHubService
 from .analyzer_service import AnalyzerService
+from .activity_service import ActivityService
 from .documentation_service import DocumentationService
 from .maintainability_service import MaintainabilityService
 from .overall_score_service import OverallScoreService
