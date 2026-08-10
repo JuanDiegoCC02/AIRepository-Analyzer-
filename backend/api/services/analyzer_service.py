9f77bb47151f662921ad8d1787ac61d073f97296
Juan Diego C.C. <dcorella2324@gmail.com>
2026-08-09 22:01:04 -0600

    fix: update build_reponse_scores to the AnalyzerService structure in analyzer_service.py archive in api/services.

--- backend/api/services/analyzer_service.py
+++ backend/api/services/analyzer_service.py
@@ -119,11 +119,11 @@ class AnalyzerService:
 
             "maintainability": analysis.maintainability_score,
 
-            "overall": analysis.overall_score,
-
             "code_quality": analysis.code_quality_score,
 
             "community": analysis.community_score,
+
+            "overall": analysis.overall_score,
 
             "level": level,
         }
