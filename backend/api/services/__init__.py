8c868b704e4157d26fba2b4131c8854536eb5850
Juan Diego C.C. <dcorella2324@gmail.com>
2026-08-10 19:09:59 -0600

    fix: update the imports for __init__.py in api/services

--- backend/api/services/__init__.py
+++ backend/api/services/__init__.py
@@ -21,6 +21,6 @@ from .repository_maturity_service import RepositoryMaturityService
 from .release_analysis_service import ReleaseAnalysisService
 from .analysis_score_service import AnalysisScoreService
 from .analysis_persistence_service import AnalysisPersistenceService
-
+from .analysis_result_service import AnalysisResultService
 
 
