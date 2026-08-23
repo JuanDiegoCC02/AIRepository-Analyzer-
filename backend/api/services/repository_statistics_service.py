1bb23a3e6a793b92fa78dc2f90bdf59fd81a6e6d
Juan Diego C.C. <dcorella2324@gmail.com>
2026-08-22 19:00:20 -0600

    feat: create the new structure get_repository_data in RepositoryStatisticsService in the repository_statistics_service.py archive in api/services.

--- backend/api/services/repository_statistics_service.py
+++ backend/api/services/repository_statistics_service.py
@@ -1,7 +1,41 @@
-
+from api.services.github_service import GitHubService
 
 
 class RepositoryStatisticsService:
+
+    """
+    Servicce reponsible for generating structured statiscs for a GitHub Repository.
+    """
+
+    classmethod 
+    def get_repository_data(cls, owner, repository,):
+
+        """
+        Retrieves the repository info required to generate statistics.
+        GitHub communication is delegated to GitHubService.
+        """
+
+        endpoint = (
+            f"/repos/" 
+            f"{owner}" 
+            f"{repository}"
+        )
+
+        try:
+            repository_data = GitHubService.request(
+                endpoint
+            )
+        except Exception:
+            return None
+
+        if not isinstance(
+            repository_data,
+            dict
+        ):
+            return None
+
+        return repository_data
+    
 
     @staticmethod
     def generate (repository):
