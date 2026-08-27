b5fe09e92fce158d2d8a9a17b15ec8b1755c2944
Juan Diego C.C. <dcorella2324@gmail.com>
2026-08-27 17:56:21 -0600

    fix: update the structure (class)RepositorySerializer for the repository_serialzer.py archive in api/serializers.

--- backend/api/serializers/repository_serializer.py
+++ backend/api/serializers/repository_serializer.py
@@ -4,12 +4,18 @@ from api.models.repository import Repository
 
 class RepositorySerializer(serializers.ModelSerializer):
 
+
+
     class Meta: 
+
         model = Repository
-        fields = "__all__"
-        read_only_fields = (
+
+        fields = [
             "id",
+            "owner",
+            "name",
+            "url",
             "created_at", 
             "updated_at", 
-            )
+        ]
         
