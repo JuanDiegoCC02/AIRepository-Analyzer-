0e62ae0d88874ca1bb4fd9b3c3b1d279e664502b
Juan Diego C.C. <dcorella2324@gmail.com>
2026-09-14 19:02:37 -0600

    fix: update of the get_repository_data in the RepositoryStatisticsService structure for the repository_statistics_service.py archive in the api/services.

--- backend/api/services/repository_statistics_service.py
+++ backend/api/services/repository_statistics_service.py
@@ -1,40 +1,25 @@
 from api.services.github_service import GitHubService
 
 
-class RepositoryStatisticsService:
 
-    """
-    Servicce reponsible for generating structured statiscs for a GitHub Repository.
-    """
+class RepositoryStatisticsService:
 
     @classmethod 
     def get_repository_data(cls, owner, repository,):
 
-        """
-        Retrieves the repository info required to generate statistics.
-        GitHub communication is delegated to GitHubService.
-        """
-
-        endpoint = (
-            f"/repos/" 
-            f"{owner}/" 
-            f"{repository}"
-        )
+        endpoint = f"/repos/{owner}/{repository}"
 
         try:
-            repository_data = GitHubService.request(
-                endpoint
-            )
+            repository_data = GitHubService.request(endpoint)
+
         except Exception:
             return None
 
-        if not isinstance(
-            repository_data,
-            dict
-        ):
+        if not isinstance(repository_data, dict):
             return None
 
         return repository_data
+
     
 
     @staticmethod
@@ -115,6 +100,7 @@ class RepositoryStatisticsService:
                 False
             ),
         }
+
 
 
     @classmethod
